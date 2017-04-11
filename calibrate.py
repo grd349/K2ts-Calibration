51fb24d2e3fe5396bdc3990b7d0cc3ceac84ff10
grd349 <grd349@gmail.com>
2017-04-11 09:07:41 +0100

    Ran the calibration

--- calibrate.py
+++ calibrate.py
@@ -9,15 +9,15 @@ from astropy.stats import mad_std
 
 if __name__ == "__main__":
     results = pd.read_csv('calibration_sample.csv')
-    results['LogNumax'] = np.log(results.Numax)
+    results['LogNumax'] = np.log10(results.Numax)
     print(results.columns)
     rg = results[results.Numax > 0]
     other = results[results.Numax < 0]
     
     x = rg.LogNumax.values
 #    y = np.log(rg.Metric1.values + rg.Metric2.values + rg.Metric3.values)
-    y = np.log(rg.medlf - rg.w100)
-    res = np.polyfit(x, y, 3)
+    y = np.log10(rg.medlf - rg.w100)
+    res = np.polyfit(x[x > 0.8], y[x > 0.8], 2)
     print(res)
     
     fig = plt.figure()
@@ -25,15 +25,42 @@ if __name__ == "__main__":
     
     ax1 = fig.add_subplot(gs[0])
 
-    ax1.scatter(x, y, facecolor='none', edgecolor='m', s=20)
+    ax1.scatter(x, y, facecolor='none', edgecolor='k', \
+                alpha=0.5, s=20, label='Data')
     xind = np.linspace(x.min(), x.max(), 100)
     fit = np.poly1d(res)(xind)
-    ax1.plot(xind, fit, 'r--', lw=3)
+    ax1.plot(xind, fit, 'r--', lw=3, label='Fit {:.2f} x**2 + {:.2f}x + {:.2f}'.format(*res))
+    ax1.set_xlabel('Log10(Numax)')
+    ax1.set_ylabel('Granulation Metric')
+    ax1.legend(loc=3)
     
     ax2 = fig.add_subplot(gs[1])
     resid = (y) - (np.poly1d(res)(x))
-    ax2.hist(resid, normed=1, bins=np.linspace(-3,3,50))
+    sns.kdeplot(resid, shade=True, ax=ax2)
+    ax2.set_xlim([-1,1])
+    ax2.set_xlabel('Resid')
     
+    rg['resid'] = y - np.poly1d(res)(x)
+    ax3 = fig.add_subplot(gs[2])
+    ax3.scatter(rg.Numax, rg.resid, c='k', alpha=0.2)
+    CS = ax3.scatter(rg.Numax[rg.M > 0], rg.resid[rg.M > 0], \
+                     c=rg.M[rg.M > 0], \
+                     cmap='copper', \
+                     vmin=0.1)
+    cbar = fig.colorbar(CS)
+    cbar.ax.set_ylabel('Mass')
+    ax3.set_ylim([-1,1])
+    ax3.set_xlabel('Numax')
+    ax3.set_ylabel('Resid')
+    ax3.set_xlim([0,250])
+    ax4 = fig.add_subplot(gs[3])
+    
+    sns.kdeplot(rg.M[rg.M > 0], rg[rg.M > 0].resid, nlevels=10, ax=ax4)
+    ax4.set_xlim([0.5,3])
+    ax4.set_ylim([-0.5,0.5])
+
+    plt.tight_layout()
+    fig.savefig('calibration.png')
     print("Bias : {:.4f}".format(np.median(resid)))
     print("Scatter : {:.4f}".format(np.std(resid)))
     print("Robust Scatter : {:.4f}".format(mad_std(resid)))
